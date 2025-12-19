c9a063a10588079f113ee43c16e09dd4157340b5
jamisontihey572-ai <jamisontihey572@gmail.com>
2025-12-19 17:04:38 -0500

    Update loader.lua
    Roblox

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 -- COPY THIS AND PASTE IT IN YOUR EXECUTOR 👇
-loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/loader2.lua", true))()
+loadstring(game:HttpGet("ohttps://raw.githubusercontent.com/VapeVoidware/VW-Add/main/loader2.lua", true))()
